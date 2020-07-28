302702aa8c655bf406d0958a8de069436c9d9653
Juan Carlos Heredia Mayer <jchm@infoinnova.net>
2020-07-28 15:06:50 +0200

    Add project files.

---
+++ TodoApi/Controllers/OVMSController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OVMSController : Controller
+    {
+        // GET: OVMS
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Index()
+        {
+            return new string[] { DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString() };
+        }
+    }
+}
