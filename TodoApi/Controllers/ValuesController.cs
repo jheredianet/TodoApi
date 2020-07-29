9f767150e00312be532f0db9cc71ec813abb6c29
jheredianet <jchm@infoinnova.net>
2020-07-29 08:22:50 +0200

    Environment.MachineName

--- TodoApi/Controllers/ValuesController.cs
+++ TodoApi/Controllers/ValuesController.cs
@@ -14,32 +14,19 @@ namespace TodoApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { DateTime.Now.ToString(), Environment.MachineName };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<string> Get(int id, string tlm, string token)
         {
-            return "value";
+            if (id!=0)
+            {
+                return BadRequest();
+            }
+            return Ok();
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
